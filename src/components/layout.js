849ab641c2204f5d97e8cea07304fae429be25a3
Michael Silveira <gravitypersists@gmail.com>
2021-08-26 13:03:02 -0400

    give a little more screen space on mobile

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,5 +1,6 @@
 import React from "react"
 import PropTypes from "prop-types"
+import { isMobile } from "react-device-detect"
 import "semantic-ui-css/semantic.min.css"
 import "./layout.css"
 
@@ -16,9 +17,9 @@ const Layout = ({ children, backgroundColor, fontFamily, style }) => {
     >
       <div
         style={{
-          margin: `0 auto`,
+          margin: "0 auto",
           maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
+          padding: isMobile ? 0 : "0 1.0875rem 1.45rem",
         }}
       >
         <main>{children}</main>
